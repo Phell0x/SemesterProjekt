b27d1c4d0397f470746f1d7eedac8a9858d2081b
Phell0x <kaihultmann@10.18.137.53>
2014-04-01 10:56:37 +0200

    Done - 5.0.1	No errors - Price made  TEST

---
+++ Casablanca Hotel/src/casablanca/booking/BookingDetail.java
@@ -0,0 +1,86 @@
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//package casablanca.booking;
+//
+///**
+//*
+//*
+//* @author kaihultmann
+//*/
+//
+//public class bookingDetail {
+//          private int Phone; 
+//	  private int CheckOutDate; 
+//	  private int CheckInDate;
+//          private String GuestName;
+//          private String GuestLastName;
+//          private String GuestAgency;
+//          private String GuestEmail;
+//          private String GuestAddress;
+//          private String GuestCountry;
+//       
+//          
+//	  public bookingDetail(int p, int ci, int co, String NameGuest, String GuestLastName, String AgencyGuest, String EmailGuest, String AddressGuest, String CountryGuest)
+//	  {
+//	Phone = p;
+//        CheckInDate = ci;
+//        CheckOutDate = co;
+//        GuestName = NameGuest;
+//        GuestAgency = AgencyGuest;
+//        GuestEmail = EmailGuest;
+//        GuestAddress = AddressGuest;
+//        GuestCountry = CountryGuest;
+//	  }
+//	  
+//	  
+//	  public int getPhone()
+//	  {
+//	    return Phone;
+//	  }
+//	  public int getCheckInDate()
+//	  {
+//	    return CheckInDate;
+//	  }
+//
+//	  public int getCheckOutDate()
+//	  {
+//	    return CheckOutDate;
+//	  }
+//          
+//        public String getGuestEmail() {
+//        return GuestEmail;
+//        
+//    }
+//        
+//        public String getGuestAgency() {
+//        return GuestAgency;
+//        
+//    }
+//        
+//        public String getGuestAddress() {
+//        return GuestAddress;
+//    }
+//    
+//        public String getGuestName() {
+//        return GuestName;
+//        
+//        }
+//        public String getGuestLastName() {
+//        return GuestLastName;
+//        
+//        }
+//        
+//        public String getGuestCountry() {
+//        return GuestCountry;
+//    }
+//        
+//         public String toString()
+//	  {
+//	    return Phone + " " + CheckOutDate + " " + CheckInDate + " " + GuestName + " " + GuestLastName + 
+//                    " " + GuestEmail + " " + GuestAddress + " " + GuestCountry + " " + 
+//                    GuestAgency;
+//	  }
+//}
